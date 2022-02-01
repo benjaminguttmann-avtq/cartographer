d3d4ad6dee5651c157b67033e55c567ac9629d29
Rasheed Abdul-Aziz <squeedee@users.noreply.github.com>
2022-02-01 17:38:25 -0500

    Initial commit, just monaco and svelte powered by vite

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import Monaco from "./lib/Monaco.svelte";
+</script>
+
+<main>
+  <h1>Cartographer Live Editor</h1>
+
+  <Monaco   />
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  h1 {
+    text-align: center;
+    color: #3e00ff;
+    text-transform: uppercase;
+    font-size: 2rem;
+    font-weight: 200;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/lib/Monaco.svelte
@@ -0,0 +1,33 @@
+<script>
+
+    import {onMount} from "svelte";
+    import * as monaco from 'monaco-editor'
+
+    const defaultText = ["---",
+        "hi: there",
+        "test: syntax",
+        "  - this: should",
+        "    be: an",
+        "    be: error",
+    ]
+
+    let inst
+    let editor
+    onMount(() => {
+        inst = monaco.editor.create(
+            editor,
+            {value: defaultText.join("\n"), language: "yaml"}
+        )
+
+        // todo: destructor
+    })
+
+</script>
+
+<div class="monaco-editor-container" bind:this={editor} style="height: 400px"></div>
+
+<style>
+    .monaco-editor-container {
+        outline: lightgray solid 1px;
+    }
+</style>
