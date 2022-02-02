99227ce108c635b0e1b5b15de2c9a4b0107b186a
Rasheed Abdul-Aziz <squeedee@users.noreply.github.com>
2022-02-01 22:00:09 -0500

    Added monaca-yaml by compiling the latests
    -- need to get the next release, or something.

--- src/lib/Monaco.svelte
+++ src/lib/Monaco.svelte
@@ -1,7 +1,70 @@
-<script>
+<script type="module">
 
     import {onMount} from "svelte";
-    import * as monaco from 'monaco-editor'
+    import {setDiagnosticsOptions} from '../monaco-yaml';
+    import {editor, Uri} from 'monaco-editor'
+    import YamlWorker from '../monaco-yaml/yaml.worker?worker';
+    import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+    import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+
+    let inst
+    let editorContainer
+
+    window.MonacoEnvironment = {
+        getWorker(moduleId, label) {
+            switch (label) {
+                case 'yaml':
+                    return new YamlWorker();
+                case 'javascript':
+                    return new TsWorker();
+                default:
+                    return new EditorWorker();
+            }
+        },
+    };
+
+    // The uri is used for the schema file match.
+    const modelUri = Uri.parse('a://b/foo.yaml');
+
+    setDiagnosticsOptions({
+        enableSchemaRequest: true,
+        hover: true,
+        completion: true,
+        validate: true,
+        format: true,
+        schemas: [
+            {
+                // Id of the first schema
+                uri: 'http://myserver/foo-schema.json',
+                // Associate with our model
+                fileMatch: [String(modelUri)],
+                schema: {
+                    type: 'object',
+                    properties: {
+                        p1: {
+                            enum: ['v1', 'v2'],
+                        },
+                        p2: {
+                            // Reference the second schema
+                            $ref: 'http://myserver/bar-schema.json',
+                        },
+                    },
+                },
+            },
+            {
+                // Id of the first schema
+                uri: 'http://myserver/bar-schema.json',
+                schema: {
+                    type: 'object',
+                    properties: {
+                        q1: {
+                            enum: ['x1', 'x2'],
+                        },
+                    },
+                },
+            },
+        ],
+    });
 
     const defaultText = ["---",
         "hi: there",
@@ -11,20 +74,23 @@
         "    be: error",
     ]
 
-    let inst
-    let editor
     onMount(() => {
-        inst = monaco.editor.create(
-            editor,
-            {value: defaultText.join("\n"), language: "yaml"}
+        inst = editor.create(editorContainer,
+            {
+                automaticLayout: true,
+                // value: defaultText.join("\n"),
+                // language: "yaml",
+                model: editor.createModel(defaultText.join("\n"), 'yaml', modelUri),
+            },
         )
 
         // todo: destructor
     })
 
+
 </script>
 
-<div class="monaco-editor-container" bind:this={editor} style="height: 400px"></div>
+<div class="monaco-editor-container" bind:this={editorContainer} style="height: 400px"></div>
 
 <style>
     .monaco-editor-container {
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
-import monacoEditorPlugin from "vite-plugin-monaco-editor"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         svelte(),
-        monacoEditorPlugin.default({
-           languageWorkers:['editorWorkerService']
-        }),
     ],
-    clearScreen: false
+    clearScreen: false,
 })
